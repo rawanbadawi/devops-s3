a7ca0aa4b0e9627ddae62eae989d79b9292f1ced
Rawan Badawi <rawanbadawi@google.com>
2023-04-17 07:46:33 -0700

    removing debugging adding new bucket

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "gcs" {
   source     = "terraform-google-modules/cloud-storage/google"
   version    = "~> 3.4"
   project_id = var.project_id
-  names      = ["${var.name}-${var.environment}"]
+  names      = ["${var.name}-${var.environment}", "${var.name}s-${var.environment}"]
   prefix     = "test"
   location   = var.location
 }
